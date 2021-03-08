44225cf247491358718af413bae5c483cbbd939e
Ehimare369 <ehimare369@gmail.com>
2021-03-08 15:26:44 +0100

    finished with the navbar component

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -5,29 +5,29 @@ import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <nav class="nav">
-      <ul class="nav-items">
-        <li class="nav-item">
+    <nav className="NavBar">
+      <ul className="nav-items">
+        <li className="nav-item">
           <NavLink to="/" exact activeClassName="active">
             Home
           </NavLink>
         </li>
-        <li class="nav-item">
+        <li className="nav-item">
           <NavLink to="/about" exact activeClassName="active">
             About
           </NavLink>
         </li>
-        <li class="nav-item">
+        <li className="nav-item">
           <NavLink to="/portfolio" exact activeClassName="active">
             Portfolio
           </NavLink>
         </li>
-        <li class="nav-item">
+        <li className="nav-item">
           <NavLink to="/blog" exact activeClassName="active">
             Blog
           </NavLink>
         </li>
-        <li class="nav-item">
+        <li className="nav-item">
           <NavLink to="/contact" exact activeClassName="active">
             Contact
           </NavLink>
