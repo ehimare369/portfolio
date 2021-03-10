deab8270beb7ee838d221bf0b77336a948e999a0
Ehimare369 <ehimare369@gmail.com>
2021-03-10 04:25:41 +0100

    coded the routing path using the layout in app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
+import { Switch, Route } from "react-router-dom";
 import MainLayout from "./layout/MainLayout/MainLayout";
+import HomePage from "./pages/HomePage/HomePage";
+import AboutPage from "./pages/AboutPage/AboutPage";
+import PortfolioPage from "./pages/PortfolioPage/PortfolioPage";
+import BlogPage from "./pages/BlogPage/BlogPage";
+import ContactPage from "./pages/ContactPage/ContactPage";
 import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <MainLayout />
-    </div>
+    <>
+      <Switch>
+        <MainLayout>
+          <Route path="/" exact>
+            <HomePage />
+          </Route>
+          <Route path="/about" exact>
+            <AboutPage />
+          </Route>
+          <Route path="/portfolio" exact>
+            <PortfolioPage />
+          </Route>
+          <Route path="/blog" exact>
+            <BlogPage />
+          </Route>
+          <Route path="/contact" exact>
+            <ContactPage />
+          </Route>
+        </MainLayout>
+      </Switch>
+    </>
   );
 }
 
--- src/layout/MainLayout/MainLayout.js
+++ src/layout/MainLayout/MainLayout.js
@@ -2,13 +2,11 @@ import React from "react";
 import "./MainLayout.scss";
 import SideBar from "./../.././components/SideBar/SideBar";
 
-const MainLayout = () => (
-  <div>
-    <div>
-      <SideBar />
-    </div>
-    <div className="MainLayout">MainLayout Component</div>
-  </div>
+const MainLayout = ({ children }) => (
+  <>
+    <SideBar />
+    <div className="MainLayout">{children}</div>
+  </>
 );
 
 export default MainLayout;
